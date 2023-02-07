925878a1146d38e658a9cb2335aede111adee520
Mericorn <106124647+Mericorn@users.noreply.github.com>
2023-02-07 18:03:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function (data_uri) {
-        
-    document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
     });
 }
 
 console.log('ml5 version:', ml5.version);
+
 classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/W1bmqDXpP/model.json', modelLoaded);
 function modelLoaded(){
     console.log("Model Loaded");
