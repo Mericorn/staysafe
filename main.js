2ef652e56c96035e511f558190d1f833ca1b99d7
Mericorn <106124647+Mericorn@users.noreply.github.com>
2023-02-07 18:02:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function (data_uri) {
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+        
+    document.getElementById("result").innerHTML= '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
