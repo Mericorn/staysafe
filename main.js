300f171559d92a0ec78ca8e4a08f91543e5f6aae
Mericorn <106124647+Mericorn@users.noreply.github.com>
2023-02-09 18:53:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/W1bmqDXpP/model.json', modelLoaded);
+classifier = ml5.imagegClassifier('https://teachablemachine.withgoogle.com/models/W1bmqDXpP/model.json', modelLoaded);
 function modelLoaded(){
     console.log("Model Loaded");
 }function check() {
